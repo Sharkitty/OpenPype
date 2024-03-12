4d5822ed81d88b40f468a82e3a2ef0eb2f53481b
Sharkitty <hayleylalicorne@gmail.com>
2024-03-12 09:14:27 -0400

    renamed hook and util script, improved logging, use source_filepath

--- openpype/hosts/blender/hooks/pre_set_current_time_to_workfile.py
+++ openpype/hosts/blender/hooks/pre_set_current_time_to_workfile.py
@@ -5,11 +5,12 @@ from openpype.hosts.blender import utility_scripts
 
 
 class PreSetWorkfilePublishTime(PreLaunchHook):
-     app_groups = [
-             "blender",
-         ]
+    app_groups = [
+        "blender",
+    ]
+
     def execute(self):
-        if self.data.get("set_workfile_publish_time"):
+        if self.data.get("source_filepath"):
             # Add `set_workfile_publish_time` script to launch arguments
             self.launch_context.launch_args.insert(
                 self.launch_context.launch_args.index("-P"),
--- openpype/hosts/blender/utility_scripts/set_current_time_to_workfile.py
+++ openpype/hosts/blender/utility_scripts/set_current_time_to_workfile.py
@@ -4,10 +4,12 @@ from openpype.lib.log import Logger
 from openpype.lib.dateutils import get_timestamp
 
 if __name__ == "__main__":
+    current_time = get_timestamp()
+
     log = Logger().get_logger()
-    log.debug("Setting workfile last publish time")
+    log.debug(f"Setting workfile last publish time to {current_time}")
 
     # Set last publish time to current time
     # This script assumes it is only executed in a known up to date workfile
     # TODO Use UTC time instead of local time
-    bpy.context.scene["op_published_time"] = get_timestamp()
+    bpy.context.scene["op_published_time"] = current_time
